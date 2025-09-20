09518c0cdf03560e4807a17dbb1ebd8b1c734f9b
anuragsrma <anuragsharma0140@gmail.com>
2025-09-20 12:41:37 +0530

    navbar complete

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -9,14 +9,14 @@ export const AppContextProvider = ({children}) => {
     const navigate = useNavigate();
     const [user , setUser] = useState(null);
     const [isSeller , setIsSeller] = useState(false);
-
-
-    const value = {navigate , user , setUser , isSeller , setIsSeller};
+    const [showUserLogin , setShowUserLogin] = useState(false);
+     
+    const value = {navigate , user , setUser , isSeller , setIsSeller , showUserLogin , setShowUserLogin};
   return<AppContext.Provider value={value} >
     {children}
   </AppContext.Provider >    
 }
 
 export const useAppContext = () => {
-    return useContext(AppContext);
+    return useContext(AppContext);    
 }
