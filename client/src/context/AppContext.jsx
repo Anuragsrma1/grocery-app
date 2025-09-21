a8a6d2bd63e24fbe84e3ba0c773fa52830b95c3d
anuragsrma <anuragsharma0140@gmail.com>
2025-09-21 16:21:22 +0530

    Add MainBanner component and integrate it into Home page; update App component for routing

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -14,7 +14,7 @@ export const AppContextProvider = ({children}) => {
     const value = {navigate , user , setUser , isSeller , setIsSeller , showUserLogin , setShowUserLogin};
   return<AppContext.Provider value={value} >
     {children}
-  </AppContext.Provider >    
+  </AppContext.Provider >     
 }
 
 export const useAppContext = () => {
